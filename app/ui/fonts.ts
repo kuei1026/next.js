eb3daf659bc3581cc9fe48b59f1705e935d79aba
unknown <m11002121@mail.ntust.edu.tw>
2025-11-11 16:24:40 +0800

    Initial commit: Next.js Dashboard project

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+// This file to keep fonts that are used across the app
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
